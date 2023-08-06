bab6c68787aede9046aef9ec26f15da0db3e7c9c
amiyaranjansahoo <meamiyaranjan@gmail.com>
2023-08-06 10:57:40 +0530

    Update for.sh

--- loop/for.sh
+++ loop/for.sh
@@ -1,1 +1,19 @@
+# Example simple demo
+#!/bin/bash
 
+for i in 1 2 3 4 5
+do
+    echo $i
+done
+
+# Example to 
+#!/bin/bash
+
+for file in /path/to/directory/*
+do
+	if [ -f "$file" ]
+	then
+		echo "Processing file: $file"
+        # Add your desired action here, such as copying, moving, or processing the file
+    fi
+done
