d04daf8c65d5fe20bc5f15ed730cc58c16ebb5c7
amiyaranjansahoo <meamiyaranjan@gmail.com>
2023-08-06 10:40:08 +0530

    Update while.sh

--- loop/while.sh
+++ loop/while.sh
@@ -6,3 +6,27 @@ do
 	echo $i
 	i=`expr $i + 1`
 done
+
+
+# Infinite loop
+while :
+do
+	echo "Demo for infinite loop" >> /tmp/output.txt
+        sleep 2
+done
+
+# Infinite loop
+while true
+do
+	echo "Demo for infinite loop" >> /tmp/output.txt
+        sleep 2
+done
+
+# Infinite loop
+while false
+do
+	echo "Demo for infinite loop" >> /tmp/output.txt
+        sleep 2
+done
+
+
